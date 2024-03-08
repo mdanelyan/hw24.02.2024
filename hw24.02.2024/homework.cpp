19c8bca8d411a6af93238f6c5443ab456b5c87f8
1987m <1987m@MDANELYANPC>
2024-03-08 11:19:32 +0500

    Add project files.

---
+++ hw24.02.2024/homework.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <functional>
+#include <iomanip>
+
+//Функторы
+
+
+
+class FormulaSpeed {
+public:
+    void operator()(double distance, double time) {
+        double result = distance / time;
+        std::cout << std::fixed << std::showpoint;
+        std::cout << std::setprecision(digit) << result << std::endl;
+    }
+    FormulaSpeed(int dig)
+    {
+       digit = dig;
+    }
+
+    int getdig() {
+        return digit;
+    }
+    int setdig(int dig) {
+        digit = dig;
+    }
+    
+private:
+    int digit;
+};
+
+class FormulaCurrentYield {
+public:
+public:
+    void operator()(double priceincrease, double dividendpaid, double currentprice) {
+        double result = (priceincrease+ dividendpaid)/currentprice;
+        std::cout << std::fixed << std::showpoint;
+        std::cout << std::setprecision(digit) << result << std::endl;
+    }
+
+
+    FormulaCurrentYield(int dig)
+    {
+        digit = dig;
+    }
+
+    int getdig() {
+        return digit;
+    }
+    int setdig(int dig) {
+        digit = dig;
+    }
+
+private:
+    int digit;
+};
+
+
+int main()
+{
+    FormulaSpeed Speed(1);
+    Speed(120, 4.34);
+    FormulaCurrentYield Yield(2);
+    Yield(20, 10, 130);
+
+}
